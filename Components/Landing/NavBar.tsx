0d4cc939caba6ef982d6344fe91478f49d5504cc
Jorim Soika <jorim.soika@gmx.de>
2025-01-10 18:26:09 +0100

    Feat: Added Navbar for landing page + added reusable button component with variants

---
+++ Components/Landing/NavBar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../Global/Button";
+
+function NavBar () {
+    return (
+        <div className="w-full flex justify-center items-center fixed top-[27px]">
+            <div className="w-[90%] max-w-[880px] sm:w-[94%] bg-[#ffffff16] rounded-full backdrop-blur-md border-2 border-[#ffffff16] px-[24px] py-[12px]">
+                <div className="flex justify-between items-center w-full">
+                    <Link className="flex gap-[10px] items-center" href="/">
+                        <Image src="/logo.svg" width={35} height={35} className="cursor-pointer" alt="FriendsCodes Logo"/>
+                        <p className="text-white font-[550] text-[20px] sm:hidden">FriendsCodes</p>
+                    </Link>
+                    <div className="flex gap-[34px] md:hidden">
+                        <Link className="text-white hover:opacity-75 font-[550]" href={'/#how-it-works'}>How it works</Link>
+                        <Link className="text-white hover:opacity-75 font-[550]" href={'/#about'}>About</Link>
+                        <Link className="text-white hover:opacity-75 font-[550]" href={'/#blog'}>Blog</Link>
+                    </div>
+                    <div className="flex gap-[15px]">
+                        <Button variant={'secondary'} size={'sm'}>Sign In</Button>
+                        <Button variant={'primary'} size={'sm'}>Get Started</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    )
+}
+
+
+export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { figtree } from "./Lib/fonts";
 
 export default {
   content: [
@@ -7,7 +8,28 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "2xl": {
+        max: '1535px'
+      },
+      "xl": {
+        max: '1279px'
+      },
+      "lg": {
+        max: '1023px'
+      },
+      "md": {
+        max: '767px'
+      },
+      "sm": {
+        max: '639px'
+      }
+    },
     extend: {
+      fontFamily: {
+        figtree: ['var(--font-figtree)'],
+        inter: ['var(--font-inter)'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
