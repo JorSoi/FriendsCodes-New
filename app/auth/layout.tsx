79486528315f38ecca7f94b09cd9bbb155969ec4
Jorim Soika <jorim.soika@gmx.de>
2025-01-30 12:48:19 +0100

    Feat: Created immediate redirect from auth pages to the dashboard, whenever a user is already logged in

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,14 @@
 // This layout component only counts for auth pages and ensures that all forms have the identical layout setting. Thus, its just here for positioning/layout purposes.
 
-function layout({children} : {children : React.ReactNode}) {
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+async function layout({children} : {children : React.ReactNode}) {
+
+    const supabase = await createClient();
+    const {data : {user}, error } = await supabase.auth.getUser();
+    if(user) redirect('/dashboard')
+
     return (
         <div className="flex h-screen w-full justify-center pt-[15vh] xs:pt-[6vh]">
         <div className="mx-[3%] xs:mx-[5%]">
